e23ef1ece4713ffa7ceab6a1d237609683ec7e77
qhtjd2131 <qhtjd2131@gmail.com>
2021-12-12 00:02:11 +0900

    component name 변경, MainItem1 -> MainItem

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import MainItem1 from "./Main-Components/MainItem1";
+import MainItem1 from "./Main-Components/MainItem";
 import Notice from "./Main-Components/Notice";
 import Promotion from "./Main-Components/Promotion";
 import Rewards from "./Main-Components/Rewards";
