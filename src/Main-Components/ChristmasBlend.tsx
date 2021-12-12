38f503f660f4e916c36c871df9b7106888effbc9
qhtjd2131 <qhtjd2131@gmail.com>
2021-12-12 21:29:49 +0900

    ChristmasBlend 기본 스타일링 완료.(애니메이션x)

--- src/Main-Components/ChristmasBlend.tsx
+++ src/Main-Components/ChristmasBlend.tsx
@@ -2,20 +2,62 @@ import styled from "styled-components";
 
 const ChristmasBlendComponent = styled.div`
   background-image: url("https://image.istarbucks.co.kr/img/event/2021/2021_christmas2_bean_bg.jpg");
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
 `;
 
 const BlendImage = styled.img`
   width: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 20px;
+`;
+
+const BlendDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 40%;
+
+  padding: 20px;
+  box-sizing: border-box;
+  gap : 20px;
+`;
+
+const BlendDescriptionTextImage = styled.img`
+  width: 100%;
+`;
+
+const BlendDescriptionButton = styled.button`
+  background-color: transparent;
+  border: 2px solid black;
+  border-radius: 8px;
+  font-size: 16px;
+  width: 130px;
+  padding: 10px;
+  font-weight: 700;
 `;
 
 const ChristmasBlend = () => {
-  return(
-  <ChristmasBlendComponent>
-    <BlendImage
-      src="https://image.istarbucks.co.kr/upload/common/img/main/2021/2021_chrsitmas_bean.png"
-      alt=""
-    />
-  </ChristmasBlendComponent>);
+  return (
+    <ChristmasBlendComponent>
+      <BlendImage
+        src="https://image.istarbucks.co.kr/upload/common/img/main/2021/2021_chrsitmas_bean.png"
+        alt=""
+      />
+      <BlendDescription>
+        <BlendDescriptionTextImage
+          src="https://image.istarbucks.co.kr/upload/common/img/main/2021/2021_chrsitmas_bean_txt.png"
+          alt=""
+        />
+        <BlendDescriptionButton>자세히 보기</BlendDescriptionButton>
+      </BlendDescription>
+    </ChristmasBlendComponent>
+  );
 };
 
 export default ChristmasBlend;
