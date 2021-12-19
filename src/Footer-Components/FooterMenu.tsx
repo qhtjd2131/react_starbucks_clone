040ee98605e0d5ef1e2f1e8095c69bfc09c4a999
qhtjd2131 <qhtjd2131@gmail.com>
2021-12-20 04:50:45 +0900

    FooterMenu Component 데이터구조 변경, 스타일링

--- src/Footer-Components/FooterMenu.tsx
+++ src/Footer-Components/FooterMenu.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import * as data from "./FooterMenuData";
 
 const FooterAwardsBox = styled.section`
   width: 100%;
@@ -6,11 +7,56 @@ const FooterAwardsBox = styled.section`
 `;
 
 const InnerWrapper = styled.div`
-  width: 70%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  box-sizing: border-box;
+  padding: 40px 80px;
+  padding-bottom: 60px;
+`;
+
+const ItemBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: white;
+`;
+const Title = styled.ul`
+  margin: 12px 0px;
+
+  list-style: none;
+  font-weight: 400;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const List = styled.li`
+  margin: 6px 0px;
+  font-size: 12px;
+  list-style: none;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 const FooterAwards = () => {
-  return <FooterAwardsBox>FOOTER</FooterAwardsBox>;
+  return (
+    <FooterAwardsBox>
+      <InnerWrapper>
+        {Object.keys(data.footerMenuData).map((_key: string, index: number) => (
+          <ItemBox key={index}>
+            <Title> {data.footerMenuData[_key].name} </Title>
+            {data.footerMenuData[_key].values.map((i: data.Ivalue) => (
+              <List>{i.title}</List>
+            ))}
+          </ItemBox>
+        ))}
+      </InnerWrapper>
+    </FooterAwardsBox>
+  );
 };
 
 export default FooterAwards;
--- src/Footer-Components/FooterMenuData.tsx
+++ src/Footer-Components/FooterMenuData.tsx
@@ -1,6 +1,24 @@
-export const footerMenuData = [
-  {
-    company: [
+interface IfooterMenuData {
+  [index: string]: Iitem;
+  company: Iitem;
+  partnership: Iitem;
+  onlineCommunity: Iitem;
+  recruit: Iitem;
+}
+
+interface Iitem {
+  name: string;
+  values: Ivalue[];
+}
+export interface Ivalue {
+  title: string;
+  links: string;
+}
+
+export const footerMenuData: IfooterMenuData = {
+  company: {
+    name: "COMPANY",
+    values: [
       {
         title: "한눈에 보기",
         links: "",
@@ -27,4 +45,60 @@ export const footerMenuData = [
       },
     ],
   },
-];
+  corporateSales: {
+    name: "CORPORATE SALES",
+    values: [
+      {
+        title: "단체 및 기업 구매 안내",
+        links: "",
+      },
+    ],
+  },
+  partnership: {
+    name: "PARTNERSHIP",
+    values: [
+      {
+        title: "신규 입점 제의",
+        links: "",
+      },
+      {
+        title: "협력 고객사 등록신청",
+        links: "",
+      },
+    ],
+  },
+  onlineCommunity: {
+    name: "ONLINE COMMUNITY",
+    values: [
+      {
+        title: "페이스북",
+        links: "",
+      },
+      {
+        title: "트위터",
+        links: "",
+      },
+      {
+        title: "유튜브",
+        links: "",
+      },
+      {
+        title: "인스타그램",
+        links: "",
+      },
+    ],
+  },
+  recruit: {
+    name: "RECRUIT",
+    values: [
+      {
+        title: "채용 소개",
+        links: "",
+      },
+      {
+        title: "채용 지원하기",
+        links: "",
+      },
+    ],
+  },
+};
