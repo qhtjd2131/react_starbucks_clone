1e243a38223f5d9b85bc38494e6f14505aeb92c3
qhtjd2131 <qhtjd2131@gmail.com>
2021-12-19 18:19:19 +0900

    FindStore Component 구성요소 구현

--- src/Main-Components/FindStore.tsx
+++ src/Main-Components/FindStore.tsx
@@ -1,8 +1,36 @@
 import styled from "styled-components";
 
-const FindStoreBox = styled.div``;
+const FindStoreBox = styled.div`
+  background-image: url("/images/store_bg.jpg");
+  width: 100%;
+  height: 400px;
+`;
+
+const ImageWrapper = styled.div``;
+
+const Image1 = styled.img``;
+const Image2 = styled.img``;
+const DecoImage1 = styled.img``;
+const DecoImage2 = styled.img``;
+
+const Text1 = styled.img``;
+const Text2 = styled.img``;
+const FindButton = styled.button``;
+
 const FindStore = () => {
-  return <FindStoreBox>find store</FindStoreBox>;
+  return (
+    <FindStoreBox>
+      <ImageWrapper>
+        <Image1 src="/images/store_exp_img01.png" alt="" />
+        <Image2 src="/images/store_exp_img02.png" alt="" />
+      </ImageWrapper>
+      <DecoImage1 src="/images/store_exp_img03.png" alt="" />
+      <DecoImage2 src="/images/store_exp_img04.png" alt="" />
+      <Text1 src="/images/store_txt01.png" alt="" />
+      <Text2 src="images/store_txt02.png" alt="" />
+      <FindButton>매장 찾기</FindButton>
+    </FindStoreBox>
+  );
 };
 
 export default FindStore;
