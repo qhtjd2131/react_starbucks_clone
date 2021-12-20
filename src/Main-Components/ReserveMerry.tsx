07a7cfd2c76c6b8bcbb1abfb842bd11a04149388
qhtjd2131 <qhtjd2131@gmail.com>
2021-12-20 23:31:40 +0900

    ReserveMerry Component에서 윈도우 크기가 커질때 위치이탈현상 수정

--- src/Main-Components/ReserveMerry.tsx
+++ src/Main-Components/ReserveMerry.tsx
@@ -5,21 +5,25 @@ import MoreInfoButton from "./MoreInfoButton";
 const ReserveMerryComponent = styled.div`
   display: flex;
   align-items: center;
+  justify-content : center;
 
   width: 100%;
   height: 400px;
   background-image: url("https://image.istarbucks.co.kr/upload/common/img/main/2021/reserve_christmas_bean_211018.jpg");
   background-position: center;
+  background-size : cover;
 
 `;
 
 const ReserveMerryItemWrapper = styled.div`
   width: 60%;
   display: flex;
+  max-width : 1300px;
   flex-direction: column;
   align-items: center;
   padding: 80px;
   gap: 30px;
+  transform : translateX(-20%);
 `;
 const ReserveMerryImage = styled.img`
   width: 100%;
